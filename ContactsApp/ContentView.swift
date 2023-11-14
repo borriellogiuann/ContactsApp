dc271abf718baeb8e8fd8999a1ba83cf4e007d55
borriellogiuann <136123294+borriellogiuann@users.noreply.github.com>
2023-11-14 12:30:35 +0100

    Update ContentView.swift

--- ContactsApp/ContentView.swift
+++ ContactsApp/ContentView.swift
@@ -34,6 +34,7 @@ struct ContentView: View {
                     }
                 }
             }
+            .navigationTitle("Contacts")
         } detail: {
             Text("Select an item")
         }
